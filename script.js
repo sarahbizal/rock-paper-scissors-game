5be7b3458cebda85d175ce704b6f744dc4ec3629
sarahbizal <sarah.bizal@gmail.com>
2026-02-04 14:07:53 -0500

    Update logic for playRound

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (humanChoice === option1 && computerChoice === option2) {
+    console.log("You lose! Paper beats rock.");
+    computerScore++;
+  } else if (humanChoice === option2 && computerChoice === option3) {
+    console.log("You lose! Scissors beats paper.");
+    computerScore++;
+  } else if (humanChoice === option3 && computerChoice === option1) {
+    console.log("You lose! Rock beats scissors.");
+    computerScore++;
+  } else {
+    console.log("You win!");
+    humanScore++;
+  }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice;
+
+playRound(humanSelection, computerSelection);
